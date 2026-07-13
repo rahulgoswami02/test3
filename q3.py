cd438cd71ea10c9e73d9e3339802d5915ace2d94
Rahul <mailtorahulgiri01@gmail.com>
2026-07-13 13:06:48 +0530

    test3

--- q3.py
+++ q3.py
@@ -1,21 +1,6 @@
-# n = int(input("Enter number of students: "))
-# marks = []
-# for i in range(n):
-#     marks.append(int(input("Enter marks: ")))
-# marks.sort()
-# print("Marks in ascending order:", marks)
-
-
-class Student:
-    def sort_marks(self):
-        n = int(input("Enter number of students: "))
-        marks = []
-
-        for i in range(n):
-            marks.append(int(input("Enter marks: ")))
-
-        marks.sort()
-        print("Marks in ascending order:", marks)
-
-s = Student()
-s.sort_marks()
+n = int(input("Enter number of students: "))
+marks = []
+for i in range(n):
+    marks.append(int(input("Enter marks: ")))
+marks.sort()
+print("Marks in ascending order:", marks)
