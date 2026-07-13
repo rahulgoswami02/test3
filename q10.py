6e8a10d41e6ccd7e471f4ea05e2a7836cae0e52a
Rahul <mailtorahulgiri01@gmail.com>
2026-07-13 13:50:26 +0530

    test3

--- q10.py
+++ q10.py
@@ -1,32 +1,27 @@
-class School:
-    def annual_report(self):
-        n = int(input("Enter number of students: "))
-        marks = []
+n = int(input("Enter number of students: "))
+marks = []
 
-        for i in range(n):
-            marks.append(int(input("Enter marks for Student" + str(i + 1) + ": ")))
+for i in range(n):
+    marks.append(int(input("Enter marks for Student" + str(i + 1) + ": ")))
 
-        marks.sort()
+marks.sort()
 
-        print("Marks in ascending order:", marks)
+print("Marks in ascending order:", marks)
 
-        mark = int(input("Enter mark to search: "))
+mark = int(input("Enter mark to search: "))
 
-        low = 0
-        high = len(marks) - 1
+low = 0
+high = len(marks) - 1
 
-        while low <= high:
-            mid = (low + high) // 2
+while low <= high:
+    mid = (low + high) // 2
 
-            if marks[mid] == mark:
-                print("Mark found at position", mid + 1)
-                break
-            elif marks[mid] < mark:
-                low = mid + 1
-            else:
-                high = mid - 1
-        else:
-            print("Mark Not Found")
-
-s = School()
-s.annual_report()
+    if marks[mid] == mark:
+        print("Mark found at position", mid + 1)
+        break
+    elif marks[mid] < mark:
+        low = mid + 1
+    else:
+        high = mid - 1
+else:
+    print("Mark Not Found")
